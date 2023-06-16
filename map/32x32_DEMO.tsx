fa32369cd9b8d57898f354f1ca5f2baf77243715
63Paulo <fernandespaul548@gmail.com>
2023-06-16 09:25:31 +0200

    PyApocalypse

---
+++ map/32x32_DEMO.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="32x32_DEMO" tilewidth="32" tileheight="32" tilecount="1040" columns="40">
+ <image source="Tileset/32x32_DEMO.png" trans="000000" width="1280" height="832"/>
+</tileset>
